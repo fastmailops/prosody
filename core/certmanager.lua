e5d19b9b0c194a0b988e3fca1fb4e5f590cdf12e
Robert Norris <rob@eatenbyagrue.org>
2013-07-30 21:36:29 +1000

    import from hg 5762:785da1854eb9

---
+++ core/certmanager.lua
@@ -0,0 +1,129 @@
+-- Prosody IM
+-- Copyright (C) 2008-2010 Matthew Wild
+-- Copyright (C) 2008-2010 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+local configmanager = require "core.configmanager";
+local log = require "util.logger".init("certmanager");
+local ssl = ssl;
+local ssl_newcontext = ssl and ssl.newcontext;
+
+local tostring = tostring;
+local pairs = pairs;
+
+local prosody = prosody;
+local resolve_path = configmanager.resolve_relative_path;
+local config_path = prosody.paths.config;
+
+local luasec_has_noticket, luasec_has_verifyext, luasec_has_no_compression;
+if ssl then
+	local luasec_major, luasec_minor = ssl._VERSION:match("^(%d+)%.(%d+)");
+	luasec_has_noticket = tonumber(luasec_major)>0 or tonumber(luasec_minor)>=4;
+	luasec_has_verifyext = tonumber(luasec_major)>0 or tonumber(luasec_minor)>=5;
+	luasec_has_no_compression = tonumber(luasec_major)>0 or tonumber(luasec_minor)>=5;
+end
+
+module "certmanager"
+
+-- Global SSL options if not overridden per-host
+local global_ssl_config = configmanager.get("*", "ssl");
+
+local core_defaults = {
+	capath = "/etc/ssl/certs";
+	protocol = "sslv23";
+	verify = (ssl and ssl.x509 and { "peer", "client_once", }) or "none";
+	options = { "no_sslv2", luasec_has_noticket and "no_ticket" or nil };
+	verifyext = { "lsec_continue", "lsec_ignore_purpose" };
+	curve = "secp384r1";
+	ciphers = "HIGH:!DSS:!aNULL@STRENGTH";
+}
+local path_options = { -- These we pass through resolve_path()
+	key = true, certificate = true, cafile = true, capath = true
+}
+
+if ssl and not luasec_has_verifyext and ssl.x509 then
+	-- COMPAT mw/luasec-hg
+	for i=1,#core_defaults.verifyext do -- Remove lsec_ prefix
+		core_defaults.verify[#core_defaults.verify+1] = core_defaults.verifyext[i]:sub(6);
+	end
+end
+
+if luasec_has_no_compression and configmanager.get("*", "ssl_compression") ~= true then
+	core_defaults.options[#core_defaults.options+1] = "no_compression";
+end
+
+function create_context(host, mode, user_ssl_config)
+	user_ssl_config = user_ssl_config or {}
+	user_ssl_config.mode = mode;
+
+	if not ssl then return nil, "LuaSec (required for encryption) was not found"; end
+
+	if global_ssl_config then
+		for option,default_value in pairs(global_ssl_config) do
+			if not user_ssl_config[option] then
+				user_ssl_config[option] = default_value;
+			end
+		end
+	end
+	for option,default_value in pairs(core_defaults) do
+		if not user_ssl_config[option] then
+			user_ssl_config[option] = default_value;
+		end
+	end
+	user_ssl_config.password = user_ssl_config.password or function() log("error", "Encrypted certificate for %s requires 'ssl' 'password' to be set in config", host); end;
+	for option in pairs(path_options) do
+		user_ssl_config[option] = user_ssl_config[option] and resolve_path(config_path, user_ssl_config[option]);
+	end
+
+	if not user_ssl_config.key then return nil, "No key present in SSL/TLS configuration for "..host; end
+	if not user_ssl_config.certificate then return nil, "No certificate present in SSL/TLS configuration for "..host; end
+
+	local ctx, err = ssl_newcontext(user_ssl_config);
+
+	-- COMPAT Older LuaSec ignores the cipher list from the config, so we have to take care
+	-- of it ourselves (W/A for #x)
+	if ctx and user_ssl_config.ciphers then
+		local success;
+		success, err = ssl.context.setcipher(ctx, user_ssl_config.ciphers);
+		if not success then ctx = nil; end
+	end
+
+	if not ctx then
+		err = err or "invalid ssl config"
+		local file = err:match("^error loading (.-) %(");
+		if file then
+			if file == "private key" then
+				file = user_ssl_config.key or "your private key";
+			elseif file == "certificate" then
+				file = user_ssl_config.certificate or "your certificate file";
+			end
+			local reason = err:match("%((.+)%)$") or "some reason";
+			if reason == "Permission denied" then
+				reason = "Check that the permissions allow Prosody to read this file.";
+			elseif reason == "No such file or directory" then
+				reason = "Check that the path is correct, and the file exists.";
+			elseif reason == "system lib" then
+				reason = "Previous error (see logs), or other system error.";
+			elseif reason == "(null)" or not reason then
+				reason = "Check that the file exists and the permissions are correct";
+			else
+				reason = "Reason: "..tostring(reason):lower();
+			end
+			log("error", "SSL/TLS: Failed to load '%s': %s (for %s)", file, reason, host);
+		else
+			log("error", "SSL/TLS: Error initialising for %s: %s", host, err);
+		end
+	end
+	return ctx, err;
+end
+
+function reload_ssl_config()
+	global_ssl_config = configmanager.get("*", "ssl");
+end
+
+prosody.events.add_handler("config-reloaded", reload_ssl_config);
+
+return _M;
---
+++ plugins/mod_tls.lua
@@ -0,0 +1,116 @@
+-- Prosody IM
+-- Copyright (C) 2008-2010 Matthew Wild
+-- Copyright (C) 2008-2010 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+local config = require "core.configmanager";
+local create_context = require "core.certmanager".create_context;
+local st = require "util.stanza";
+
+local secure_auth_only = module:get_option("c2s_require_encryption") or module:get_option("require_encryption");
+local secure_s2s_only = module:get_option("s2s_require_encryption");
+local allow_s2s_tls = module:get_option("s2s_allow_encryption") ~= false;
+
+local xmlns_starttls = 'urn:ietf:params:xml:ns:xmpp-tls';
+local starttls_attr = { xmlns = xmlns_starttls };
+local starttls_proceed = st.stanza("proceed", starttls_attr);
+local starttls_failure = st.stanza("failure", starttls_attr);
+local c2s_feature = st.stanza("starttls", starttls_attr);
+local s2s_feature = st.stanza("starttls", starttls_attr);
+if secure_auth_only then c2s_feature:tag("required"):up(); end
+if secure_s2s_only then s2s_feature:tag("required"):up(); end
+
+local hosts = prosody.hosts;
+local host = hosts[module.host];
+
+local ssl_ctx_c2s, ssl_ctx_s2sout, ssl_ctx_s2sin;
+do
+	local function get_ssl_cfg(typ)
+		local cfg_key = (typ and typ.."_" or "").."ssl";
+		local ssl_config = config.rawget(module.host, cfg_key);
+		if not ssl_config then
+			local base_host = module.host:match("%.(.*)");
+			ssl_config = config.get(base_host, cfg_key);
+		end
+		return ssl_config or typ and get_ssl_cfg();
+	end
+
+	local ssl_config, err = get_ssl_cfg("c2s");
+	ssl_ctx_c2s, err = create_context(host.host, "server", ssl_config); -- for incoming client connections
+	if err then module:log("error", "Error creating context for c2s: %s", err); end
+
+	ssl_config = get_ssl_cfg("s2s");
+	ssl_ctx_s2sin, err = create_context(host.host, "server", ssl_config); -- for incoming server connections
+	ssl_ctx_s2sout = create_context(host.host, "client", ssl_config); -- for outgoing server connections
+	if err then module:log("error", "Error creating context for s2s: %s", err); end -- Both would have the same issue
+end
+
+local function can_do_tls(session)
+	if not session.conn.starttls then
+		return false;
+	elseif session.ssl_ctx then
+		return true;
+	end
+	if session.type == "c2s_unauthed" then
+		session.ssl_ctx = ssl_ctx_c2s;
+	elseif session.type == "s2sin_unauthed" and allow_s2s_tls then
+		session.ssl_ctx = ssl_ctx_s2sin;
+	elseif session.direction == "outgoing" and allow_s2s_tls then
+		session.ssl_ctx = ssl_ctx_s2sout;
+	else
+		return false;
+	end
+	return session.ssl_ctx;
+end
+
+-- Hook <starttls/>
+module:hook("stanza/urn:ietf:params:xml:ns:xmpp-tls:starttls", function(event)
+	local origin = event.origin;
+	if can_do_tls(origin) then
+		(origin.sends2s or origin.send)(starttls_proceed);
+		origin:reset_stream();
+		origin.conn:starttls(origin.ssl_ctx);
+		origin.log("debug", "TLS negotiation started for %s...", origin.type);
+		origin.secure = false;
+	else
+		origin.log("warn", "Attempt to start TLS, but TLS is not available on this %s connection", origin.type);
+		(origin.sends2s or origin.send)(starttls_failure);
+		origin:close();
+	end
+	return true;
+end);
+
+-- Advertize stream feature
+module:hook("stream-features", function(event)
+	local origin, features = event.origin, event.features;
+	if can_do_tls(origin) then
+		features:add_child(c2s_feature);
+	end
+end);
+module:hook("s2s-stream-features", function(event)
+	local origin, features = event.origin, event.features;
+	if can_do_tls(origin) then
+		features:add_child(s2s_feature);
+	end
+end);
+
+-- For s2sout connections, start TLS if we can
+module:hook_stanza("http://etherx.jabber.org/streams", "features", function (session, stanza)
+	module:log("debug", "Received features element");
+	if can_do_tls(session) and stanza:child_with_ns(xmlns_starttls) then
+		module:log("debug", "%s is offering TLS, taking up the offer...", session.to_host);
+		session.sends2s("<starttls xmlns='"..xmlns_starttls.."'/>");
+		return true;
+	end
+end, 500);
+
+module:hook_stanza(xmlns_starttls, "proceed", function (session, stanza)
+	module:log("debug", "Proceeding with TLS on s2sout...");
+	session:reset_stream();
+	session.conn:starttls(session.ssl_ctx);
+	session.secure = false;
+	return true;
+end);
